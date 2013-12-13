2a836b4718ae48f424995302f2ad9efd70f0bdc5
joamag <joamag@gmail.com>
2013-12-13 12:00:54 +0000

    new naming changes and small typeface change

--- src/pt/hive/cameo/LoginActivity.java
+++ src/pt/hive/cameo/LoginActivity.java
@@ -1,9 +1,12 @@
 package pt.hive.cameo;
 
-import pt.hive.caneo.R;
+import pt.hive.cameo.R;
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Typeface;
+import android.text.method.PasswordTransformationMethod;
 import android.view.Window;
+import android.widget.EditText;
 
 public class LoginActivity extends Activity {
 
@@ -15,5 +18,11 @@ public class LoginActivity extends Activity {
         // and then sets the login layout on it (starts the layout)
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.setContentView(R.layout.login);
+        
+        // retrieves the password edit text field and updates it to the
+        // sans serif typeface and then updates the transformation method
+        EditText password = (EditText) this.findViewById(R.id.password);
+        password.setTypeface(Typeface.SANS_SERIF);
+        password.setTransformationMethod(new PasswordTransformationMethod());
     }
 }
