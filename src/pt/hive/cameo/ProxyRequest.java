38cb319b61c4b941a72ee5eb59e4b42be22203cd
joamag <joamag@gmail.com>
2014-05-26 16:44:58 +0100

    better proxy request

--- src/pt/hive/cameo/ProxyRequest.java
+++ src/pt/hive/cameo/ProxyRequest.java
@@ -11,6 +11,8 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -19,111 +21,118 @@ import android.os.AsyncTask;
 /**
  * Abstract class responsible for the handling of remote json request that may
  * or may not require and underlying authentication process.
- *
+ * 
  * The handling of the authentication should be automatic and the proper panel
  * should be raised upon the invalidation of credentials.
- *
+ * 
  * @author João Magalhães <joamag@hive.pt>
  */
 public class ProxyRequest extends AsyncTask<Void, Void, String> {
 
-    /**
-     * The size of the buffer that is going to be used in the reading of the
-     * various message maintained by the proxy request.
-     */
-    private int BUFFER_SIZE = 4096;
+	/**
+	 * The reference to the delegate object that is going to be used for the
+	 * calling of the various callback functions for the request. This is required
+	 * in order to ensure a proper asynchronous approach.
+	 */
+	private ProxyRequestDelegate delegate;
 
-    /**
-     * The context object that is going to be used for resolution of global
-     * values this is required in order for the request to work properly.
-     */
-    private Context context;
+	/**
+	 * The context object that is going to be used for resolution of global
+	 * values this is required in order for the request to work properly.
+	 */
+	private Context context;
 
-    /**
-     * The relative URL path for the request that is going to be performed, this
-     * should be JSON based (eg: api/info.json).
-     */
-    private String path;
+	/**
+	 * The relative URL path for the request that is going to be performed, this
+	 * should be JSON based (eg: api/info.json).
+	 */
+	private String path;
 
-    /**
-     * Relative path to the URL that is going to be called in case a login
-     * operation is required, note that the login action is always going to be
-     * show for such operations.
-     */
-    private String loginPath;
+	/**
+	 * Relative path to the URL that is going to be called in case a login
+	 * operation is required, note that the login action is always going to be
+	 * show for such operations.
+	 */
+	private String loginPath;
 
-    public ProxyRequest(Context context, String path, String loginPath) {
-        this.context = context;
-        this.path = path;
-        this.loginPath = path;
-    }
+	public ProxyRequest(Context context, String path, String loginPath) {
+		this.context = context;
+		this.path = path;
+		this.loginPath = path;
+	}
 
-    @Override
-    protected String doInBackground(Void... params) {
-        try {
-            return this.load();
-        } catch (ClientProtocolException exception) {
-            exception.printStackTrace();
-        } catch (IOException exception) {
-            exception.printStackTrace();
-        }
-        return null;
-    }
+	@Override
+	protected String doInBackground(Void... params) {
+		try {
+			return this.load();
+		} catch (ClientProtocolException exception) {
+			exception.printStackTrace();
+		} catch (IOException exception) {
+			exception.printStackTrace();
+		} catch (JSONException exception) {
+			exception.printStackTrace();
+		}
+		return null;
+	}
 
-    /**
-     * Starts the loading of the request, this is considered to be the trigger
-     * of the remote call operation, the proper callback methods will be called
-     * upon the proper loading of the operations.
-     *
-     * @return The resulting string value, resulting from the loading of the
-     * @throws IOException
-     * @throws ClientProtocolException
-     */
-    public String load() throws ClientProtocolException, IOException {
-        String result = null;
+	/**
+	 * Starts the loading of the request, this is considered to be the trigger
+	 * of the remote call operation, the proper callback methods will be called
+	 * upon the proper loading of the operations.
+	 * 
+	 * @return The resulting string value, resulting from the loading of the
+	 * @throws IOException
+	 * @throws ClientProtocolException
+	 * @throws JSONException 
+	 */
+	public String load() throws ClientProtocolException, IOException, JSONException {
+		String result = null;
+		
+		SharedPreferences preferences = this.context.getSharedPreferences(
+				"cameo", Context.MODE_PRIVATE);
+		String baseUrl = preferences.getString("baseUrl", null);
+		String urlString = String.format("%s%s", baseUrl, this.path);
 
-        SharedPreferences preferences = this.context.getSharedPreferences(
-                "cameo", Context.MODE_PRIVATE);
-        String baseUrl = preferences.getString("baseUrl", null);
-        String urlString = String.format("%s%s", baseUrl, this.path);
+		HttpGet get = new HttpGet(urlString);
+		HttpClient client = new DefaultHttpClient();
+		HttpResponse response = client.execute(get);
+		HttpEntity entity = response.getEntity();
+		InputStream stream = entity.getContent();
 
-        HttpGet get = new HttpGet(urlString);
-        HttpClient client = new DefaultHttpClient();
-        HttpResponse response = client.execute(get);
-        HttpEntity entity = response.getEntity();
-        InputStream stream = entity.getContent();
+		try {
+			result = ProxyRequest.convertStreamToString(stream);
+		} finally {
+			stream.close();
+		}
+		
+		JSONObject data = new JSONObject(result);
+		if(this.delegate != null) {
+			this.delegate.didReceiveJson(data);
+		}
 
-        try {
-            result = ProxyRequest.convertStreamToString(stream);
-        } finally {
-            stream.close();
-        }
+		return result;
+	}
 
-        return result;
-    }
+	public void showLogin() {
+	}
 
-    public void showLogin() {
-    }
+	private static String convertStreamToString(InputStream stream)
+			throws IOException {
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(stream));
+		StringBuilder builder = new StringBuilder();
 
-    private static String convertStreamToString(InputStream stream) {
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(stream));
-        StringBuilder builder = new StringBuilder();
-
-        String line = null;
-        try {
-            while ((line = reader.readLine()) != null) {
-                builder.append(line + "\n");
-            }
-        } catch (IOException exception) {
-            exception.printStackTrace();
-        } finally {
-            try {
-                stream.close();
-            } catch (IOException exception) {
-                exception.printStackTrace();
-            }
-        }
-        return builder.toString();
-    }
+		try {
+			while (true) {
+				String line = reader.readLine();
+				if (line == null) {
+					break;
+				}
+				builder.append(line + "\n");
+			}
+		} finally {
+			stream.close();
+		}
+		return builder.toString();
+	}
 }
